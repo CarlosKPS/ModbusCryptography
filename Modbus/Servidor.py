d56bc3b2a5e312b6758fee305526fce9ea55d824
Carlos K. Pereira <42221416+CarlosKPS@users.noreply.github.com>
2022-03-07 06:53:01 -0300

    Add files via upload

--- Modbus/Servidor.py
+++ Modbus/Servidor.py
@@ -12,6 +12,7 @@ import random
 from time import time, time_ns
 import datetime as dt
 
+
 class ServidorModbus():
 
     def __init__(self, host_ip, port):
@@ -47,10 +48,13 @@ class ServidorModbusRSA():
         self._tabmodbus = DataBank()
         self._tabmodbus.set_bits(0, [1])
         self._crip = CifraRSA()
-        self._plainlist = PLAIN_VEC
+        # self._plainlist = PLAIN_VEC
+        self._plainlist = read_vector_from_list()  # Lê todos os vetores de 10 Bits
         self._current_plain = []
         self._i = 0
+        # Para armazenar informações
         self._timelist = []
+        self._cipher = []
 
     def run(self):
         """
@@ -63,45 +67,60 @@ class ServidorModbusRSA():
             while True:
                 # Atualizar o tabela modbus que será utilizada
                 self.update_data()
-
+                #print("Texto claro atual: ", self._current_plain)
                 # conta o tempo para cifrar a mensagem e guardar na tabela modbus
-                start_time = time()
+                start_time = time_ns()
                 # Fazer com que tenha 2000 bits
                 aux1 = self._crip.encryptRSA(self._current_plain)  # verificar a quantidade de bits que há.
                 n_zeros = [0] * (2000 - len(aux1))
                 self._tabmodbus.set_bits(0, n_zeros + aux1)
-                end_time = time()
+                end_time = time_ns()
+                print("Tempo para cifrar: ", (end_time - start_time)/1e9)
                 # guarda os valores de tempo numa lista
-                print("Tamanho da mensagem:", len(n_zeros + aux1))
-                self._timelist.append(end_time - start_time)
-                print("conjunto de bits monitoradoes: ",
-                      self._tabmodbus.get_bits(0, 2000))
+                #print("Tamanho da mensagem:", len(n_zeros + aux1))
+                self._timelist.append((end_time - start_time)/1e9)
+                #print("conjunto de bits monitoradoes: ",
+                #  self._tabmodbus.get_bits(0, 2000))
+                self._cipher.append(n_zeros + aux1)  # Cria a lista do texto cifrado
+                print(self._i)
                 sleep(2)
         except:
+            create_txt_from_vector(self._cipher, 'RSA-Modbus-Ciphertext.txt')
+            create_txt_from_vector(self._timelist, 'RSA-Modbus-TimeToEncrypt.txt')
+            print("A média para cifrar um evento é de: ", mean_from_time_vector(self._timelist))
+
             print("servidor desligado")
 
     def update_data(self):
-        self._current_plain = self._plainlist[self._i % 5]
+        self._current_plain = self._plainlist[self._i % len(self._plainlist)]
         self._i += 1
 
 
 class ServidorModbusChacha20():
 
     def __init__(self, host_ip='localhost', port=502):
+        # -------------------------------------------------------------------------------------------------------------
+        # carregando os vetores de teste
+        test_vect = read_vector_from_list()
         # ------------------------------------------------------------------------------------------------------------
         self._servidor = ModbusServer(host=host_ip, port=port, no_block=True)  # Servidor
         self._tabmodbus = DataBank()  # Tabela Modbus
-        self._tabmodbus.set_bits(0, VEC_PLAINTEX[0])
+        # self._tabmodbus.set_bits(0, VEC_PLAINTEX[0])
+        self._tabmodbus.set_bits(0, test_vect[0])
         # ------------------------------------------------------------------------------------------------------------
         self._chacha20 = Chacha20Cifra()  # Class da cifra Chacha20
         # ------------------------------------------------------------------------------------------------------------
-        self._pa = VEC_PLAINTEX[0]  # Texto claro atual
-        self._cipher = [VEC_PLAINTEX[0]]  # Texto cifrado atual
-        self._pf = []
-
         # Texto claro
-        self._plaintext = VEC_PLAINTEX  # texto claro que será feito o upload
+        # self._plaintext = VEC_PLAINTEX  # texto claro que será feito o upload
+        self._plaintext = test_vect # texto claro que será feito o upload
+
         self._i = 0  # contador auxiliar para rodar os textos
+        # -----------------------------------------------------------------------------------------------------------
+        # self._pa = VEC_PLAINTEX[0]  # Texto claro atual
+        self._pa = test_vect[0]  # Texto claro atual
+        # self._cipher = [VEC_PLAINTEX[0]]  # Texto cifrado atual
+        self._cipher = [test_vect[0]]  # Texto cifrado atual
+        self._pf = []
 
         # Lista para descobrir o tempo
         self._time_list = []
@@ -130,20 +149,24 @@ class ServidorModbusChacha20():
                 # Cifra a mensagem e coloca na tabela Modbus
                 start_time = time_ns()
                 self._tabmodbus.set_bits(0, self._chacha20.encrypt(self._pa, self._pf, self._cipher[-1]))
-                end_time = time_ns()
                 # -----------------------------
                 # adiciona o ultimo resultado na lista cipher
-                self._cipher.append(self._tabmodbus.get_bits(0, 16))
+                self._cipher.append(self._tabmodbus.get_bits(0, 10))
                 # Toma o ultimo texto claro
                 self._pa = self._pf
-                print("Ciphertext: {0}    Bits: {1}".format(self._cipher[-1], len(self._cipher[-1])))
+                end_time = time_ns()
+                #print("Ciphertext: {0}    Bits: {1}".format(self._cipher[-1], len(self._cipher[-1])))
                 self._time_list.append((end_time - start_time)/1e9)  # salvando o tempo de execução
                 # self._tabmodbus.set_bits(0, self._cipher[-1])
                 # self._tabmodbus.set_bits(0, self._cipher[-1]) # Salva os bist na tabela Modbus
                 # self._pa.append(coil_val) # atualiza os valores de Pa
-                sleep(2.5)
+                print(self._i)
+                sleep(2.3)
         except:
             print("servidor desligado")
+            create_txt_from_vector(self._cipher, 'ChaCha20-Modbus-Chipertext.txt')
+            create_txt_from_vector(self._time_list, 'ChaCha20-Modbus-TimeToEncrypt.txt')
+            print("A média para cifrar um evento é de: ", mean_from_time_vector(self._time_list))
 
     def update_data(self):
         self._pf = self._plaintext[self._i % 20]
@@ -155,3 +178,34 @@ def generate_random_vec(lenght=64):
     for i in range(0, lenght):
         vec_list.append(random.randrange(0, 2, 1))
     return vec_list
+
+
+def read_vector_from_list(n_list=100):
+    aux_list = []
+    vec_list = []
+    with open(r"plaintext.txt", 'r') as f:
+        lines = f.readlines()[:n_list]
+    for l in lines:
+        aux_list.append(l.strip("\n").strip("]").strip("[").replace(" ", "").replace(",", ""))
+    for element in aux_list:
+        vec_list.append([int(i) for i in element])
+    f.close()
+    return vec_list
+
+
+def create_txt_from_vector(vec, file_name):
+    f = open(file_name, "w+")
+    for i in vec:
+        f.write("{0}\n".format(i))
+    f.close()
+    return 0
+
+
+def mean_from_time_vector(time_vec):
+    """
+    Função para tirar a média de um vetor tirando todos os resultados iguais a zeros
+    time_vec: vetor com os tempos armazenados
+    return: uma tupla com os valores da média e o numero da amostra excluindo-se os zeros
+    """
+    aux_time_vec = [i for i in time_vec if i != 0]
+    return sum(aux_time_vec) / len(aux_time_vec), len(aux_time_vec)
--- Modbus/cliente.py
+++ Modbus/cliente.py
@@ -44,7 +44,7 @@ class SimpleModbusClient():
 
 class SimpleModbusRSAClient():
 
-    def __init__(self, host='localhost', port=502, scan_time=0.5, mensage_lenght=64):
+    def __init__(self, host='localhost', port=502, scan_time=0.5, mensage_lenght=10):
 
         self._cliente = ModbusClient(host=host, port=port)
         self._scan_time = scan_time
@@ -53,6 +53,7 @@ class SimpleModbusRSAClient():
         self._mensage_leght = mensage_lenght
         self._decrip = CifraRSA()
         self._timelist = []
+        self._timelist_read_coil = []
         self._decrption_list = []
 
     def run(self, show_iter=True):
@@ -61,38 +62,55 @@ class SimpleModbusRSAClient():
         try:
             # Starting a loop
             while True:
-                start_time = time()
-                # coil_val = [1 if i==True else 0  for i in self._cliente.read_coils(0,2000)]
+                start_time = time_ns()
+                coil_val = [1 if i == True else 0 for i in self._cliente.read_coils(0, 2000)]
+                end_time = time_ns()
+                print("Tempo para ler a mensagem: ", (end_time - start_time) / 1e9)
+                self._timelist_read_coil.append((end_time - start_time) / 1e9)  # Tempo de leitura dos 2000 bits
                 # print(len(coil_val))
-                aux_vec = self._decrip.decriptRSA([1 if i == True else 0 for i in self._cliente.read_coils(0, 2000)])
-                print(len(aux_vec))
+                # aux_vec = self._decrip.decriptRSA([1 if i == True else 0 for i in self._cliente.read_coils(0, 2000)])
+                start_time = time_ns()
+                aux_vec = self._decrip.decriptRSA(coil_val)
+                # print(len(aux_vec))
                 self._decrption_list.append([0] * (self._mensage_leght - len(aux_vec)) + aux_vec)
-                print(len(self._decrption_list[-1]))
-                end_time = time()
+                # print(len(self._decrption_list[-1]))
+                end_time = time_ns()
+                print("Tempo para decriptar: ", (end_time - start_time) / 1e9)
                 if show_iter:
                     # print("Observed coils: \n", coil_val)
-                    print("tempo de leitura", end_time - start_time)
-                    self._timelist.append(end_time - start_time)
+                    # print("tempo de leitura", end_time - start_time)
+                    self._timelist.append((end_time - start_time) / 1e9)
                 sleep(self._scan_time)
-        except Exception as e:
-            print("Error", e.args)
+        except:
+            create_txt_from_vector(self._decrption_list, 'RSA-Modbus-BackToPlaintext.txt')
+            create_txt_from_vector(self._timelist, 'RSA-Modbus-TimeToDecrypt.txt')
+            create_txt_from_vector(self._timelist_read_coil, 'RSA-Modbus-TimeToReadCiphertext.txt')
+
+            print("Tempo médio de leitura das bobinas: ", mean_from_time_vector(self._timelist_read_coil))
+            print("Tempo médio para decriptar uma mensagem:", mean_from_time_vector(self._timelist))
+
+            print("Cliente desligado")
 
 
 class ClienteModbusChacha20():
 
     def __init__(self, host='localhost', port=502, scan_time=1):
 
+        test_vect = read_vector_from_list()
+
         self._cliente = ModbusClient(host=host, port=port)
         self._scan_time = scan_time
         # ----------------------------------------------------------
         # Atributos para o processo de decifrar uma mensagem
         self._chacha20 = Chacha20Cifra()
-        self._ca = VEC_PLAINTEX[0]  # Cifra atual
+        # self._ca = VEC_PLAINTEX[0]  # Cifra atual
+        self._ca = test_vect[0]  # Cifra atual
         self._cf = []  # Cifra final
-        self._back_to_plain = [VEC_PLAINTEX[0]]  # Devolta ao texto claro
-
+        # self._back_to_plain = [VEC_PLAINTEX[0]]  # Devolta ao texto claro
+        self._back_to_plain = [test_vect[0]]  # Devolta ao texto claro
         # Medição de tempo
         self._time_list = []
+        self._timelist_read_coil = []
 
     def run(self):
         """
@@ -107,22 +125,26 @@ class ClienteModbusChacha20():
             while True:
 
                 # funcção para cifrar a mensagem
-                start_time = time_ns()
+                # start_time = time_ns()
                 # self._cf = self._cliente.read_coils(0, 16)  # Armazena na cifra atual
                 # Realiza a decriptação e realiza um append no vetor back to plain
-                print("----------------------------------------------------------------------------------------------")
-                print("ca: ", self._ca)
-                print("cf: ", self._cf)
+                # print("----------------------------------------------------------------------------------------------")
+                # print("ca: ", self._ca)
+                # print("cf: ", self._cf)
 
-                if self._cliente.read_coils(0, 16) is None:
+                if self._cliente.read_coils(0, 10) is None:
                     self._cf = self._ca
-                    print("cf: ", self._cf)
-                    print("Não inicou")
+                    # print("cf: ", self._cf)
+                    # print("Não inicou")
                 else:
-                    self._cf = [1 if i == True else 0 for i in self._cliente.read_coils(0, 16)]
-                    print("cf: ", self._cf)
-                    print("Servidor on")
+                    start_time = time_ns()
+                    self._cf = [1 if i == True else 0 for i in self._cliente.read_coils(0, 10)]
+                    end_time = time_ns()
+                    self._timelist_read_coil.append((end_time - start_time) / 1e9)
+                    # print("cf: ", self._cf)
+                    # print("Servidor on")
 
+                start_time = time_ns()
                 self._back_to_plain.append(
                     self._chacha20.decrypt(self._ca, self._cf, self._back_to_plain[-1])
                 )
@@ -132,5 +154,43 @@ class ClienteModbusChacha20():
                 self._time_list.append((end_time - start_time) / 1e9)
                 sleep(self._scan_time)
 
-        except Exception as e:
-            print("Error".e.args)
+        except:
+            create_txt_from_vector(self._back_to_plain, 'ChaCha20-Modbus-BackToPlaintext.txt')
+            create_txt_from_vector(self._time_list, 'ChaCha20-Modbus-TimeToDecrypt.txt')
+            create_txt_from_vector(self._timelist_read_coil, 'ChaCha20-Modbus-TimeToReadCiphertext.txt')
+
+            print("Tempo médio de leitura das bobinas: ", mean_from_time_vector(self._timelist_read_coil))
+            print("Tempo médio para decriptar uma mensagem:", mean_from_time_vector(self._time_list))
+
+            print("Cliente encerrado")
+
+
+def create_txt_from_vector(vec, file_name):
+    f = open(file_name, "w+")
+    for i in vec:
+        f.write("{0}\n".format(i))
+    f.close()
+    return 0
+
+
+def mean_from_time_vector(time_vec):
+    """
+    Função para tirar a média de um vetor tirando todos os resultados iguais a zeros
+    time_vec: vetor com os tempos armazenados
+    return: uma tupla com os valores da média e o numero da amostra excluindo-se os zeros
+    """
+    aux_time_vec = [i for i in time_vec if i != 0]
+    return sum(aux_time_vec) / len(aux_time_vec), len(aux_time_vec)
+
+
+def read_vector_from_list(n_list=100):
+    aux_list = []
+    vec_list = []
+    with open(r"plaintext.txt", 'r') as f:
+        lines = f.readlines()[:n_list]
+    for l in lines:
+        aux_list.append(l.strip("\n").strip("]").strip("[").replace(" ", "").replace(",", ""))
+    for element in aux_list:
+        vec_list.append([int(i) for i in element])
+    f.close()
+    return vec_list
